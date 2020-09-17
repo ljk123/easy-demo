0f751854890d2cd0d4348a783983d1434238b340
ljk <303147190@qq.com>
2020-09-17 21:35:31 +0800

    init

---
+++ web/index.php
@@ -0,0 +1,13 @@
+<?php
+namespace easy;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// 执行HTTP应用并响应
+$http = (new App())->http;
+
+$response = $http->run();
+
+$response->send();
+
+$http->end($response);
