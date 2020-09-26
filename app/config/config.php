93edb49666250baaf5c1dfd85ad9b6a77b84ccef
ljk <303147190@qq.com>
2020-09-26 12:36:07 +0800

    swoole test

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,6 @@
 <?php
 return[
     'app_debug'=>true,
-    'server_type'=>'fpm',
     'config_files'=>[//需要自动加载的配置文件
         //file=>name
     ],
--- app/controller/Hello.php
+++ app/controller/Hello.php
@@ -17,7 +17,7 @@ class Hello
         $memory=Runtime::memory()-$app->begin_memory;
         $time=Runtime::microtime()-$app->begin_time;
         return [
-            'now'=>$now[0],
+            'now'=>$app->request->get(),
             'memory'=>round($memory/1024/1024,2).'M',
             'time'=>$time
         ];
