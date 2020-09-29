943a6d17a802610c96212ba05e3dbb15e7c3dfe0
ljk <303147190@qq.com>
2020-09-29 14:20:37 +0800

    pool

--- app/config/swoole.php
+++ app/config/swoole.php
@@ -9,11 +9,15 @@ return[
         'name'=>'mysql',//区别连接池的标示
         'min_size'=>20,//每个进程启动链接数量
         'max_size'=>100,//最大连接数
+        'free_time'=>600,//空闲时间
+        'timeout'=>1,//获取连接池超时时间
     ],
     'redis'=>[
         'pool'=>true,//是否起用连接池
         'name'=>'redis',//区别连接池的标示
         'min_size'=>20,//每个进程启动链接数量
         'max_size'=>100,//最大连接数
+        'free_time'=>600,//空闲时间
+        'timeout'=>1,//获取连接池超时时间
     ],
 ];
