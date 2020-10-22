d08ad24f825f82deb57042019a4610e586cbe35b
ljk <303147190@qq.com>
2020-10-22 10:33:41 +0800

    test

--- app/controller/v1/Hello.php
+++ app/controller/v1/Hello.php
@@ -4,17 +4,28 @@
 namespace app\controller\v1;
 
 
+use app\model\User;
 use easy\App;
 use easy\Container;
 use easy\Exception;
 use easy\Log;
+use easy\Validate;
 
 class Hello
 {
-    public function index(App $app)
+    public function index(App $app,User $user)
     {
-//        var_dump($app->cache->set('aaa',121212));
-//        var_dump($app->cache->get('aaa'));
+//        $v = new Validate();
+//        $v->setAliases([
+//            'name'=>'用户名',
+//        ])->validate(['name'=>''],[
+//            'name'=>'required'
+//        ])->isOk();
+//        var_dump($v->getErrors());
+//        var_dump($app->db->table('user')->where(['id'=>1])->find());
+//        var_dump($app->db->getError());
+//        var_dump($user->where(['id'=>1])->find());
+//        var_dump($user->getError());
         return [
             'msg'=>'hello easy-php',
         ];
---
+++ app/model/User.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace app\model;
+
+
+use easy\Model;
+
+class User extends Model
+{
+
+}
